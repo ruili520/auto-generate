a639b14e1d541a8625597d93610b10820ab83de6
王文博 <wwb@echase.cn>
2023-10-11 14:02:53 +0800

    第一次提交github

---
+++ src/main/java/cn/rl520/autogenerate/AutoGenerateApplication.java
@@ -0,0 +1,13 @@
+package cn.rl520.autogenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoGenerateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/rl520/autogenerate/AutoGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.rl520.autogenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoGenerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
